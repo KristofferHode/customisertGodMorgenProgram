3c047c766994d92e2aa1e9aa81280bf9a9605b99
KristofferHode <kristoffer9.kodehode@gmail.com>
2025-10-14 13:39:55 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace customisertGodMorgenProgram;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime now = DateTime.Now;
+        //Console.WriteLine(now);
+        
+
+        Console.WriteLine("Hello, what is your name?");
+
+        String navn = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {navn}!");
+
+        
+    }
+}
